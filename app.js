09bc258ff87b290c4588b3b9b4b0f0b54c3d9801
CirehX <157507892+CirehX@users.noreply.github.com>
2024-04-26 22:08:34 -0300

    Porfolio Eric Arancibia V1

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+function irARepositorioBarberiaAlura() {
+    window.location.href = "https://github.com/CirehX/barberiaAlura";
+}
+
+function irAWebBarberiaAlura() {
+    window.location.href = "https://cirehx.github.io/barberiaAlura/";
+}
+
+
+function irARepositorioNumeroSecreto() {
+    window.location.href = "https://github.com/CirehX/Juego-secreto";
+}
+
+function irAWebNumeroSecreto() {
+    window.location.href = "https://cirehx.github.io/Juego-secreto/";
+}
+
+
+function irARepositorioEncriptador() {
+    window.location.href = "https://github.com/CirehX/Encriptador";
+}
+
+function irAWebEncriptador() {
+    window.location.href = "https://cirehx.github.io/Encriptador/";
+}
+
+
+function irARepositorioNuevaBarberia() {
+    window.location.href = "https://github.com/CirehX/NuevaBarberia";
+}
+
+function irAWebNuevaBarberia() {
+    window.location.href = "https://cirehx.github.io/NuevaBarberia/";
+}
+
+
+const nombreUsuario = document.querySelector("[name=nombre]")
